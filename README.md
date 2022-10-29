ef4e894a4ccacb763871e3570b7278b61a55bd38
Prakash Dahal <dahalprakash1720@gmail.com>
2022-10-29 20:38:28 +0545

    Initial guess game

---
+++ README.md
@@ -0,0 +1,18 @@
+# Guess the number
+
+|Levels     |Number|
+--- | --- | 
+|Easy       |10    | 
+|Medium     |50    | 
+|Hard       |100   | 
+|SuperHard  |1000  | 
+
+- A command line game.
+- Max attempt is 3.
+- Guess the number in 3 attempts.
+
+```
+npx cli-guess-game
+```
+
+*Note: You must have node and npx version greater than 12 in your machine to run this.*
---
+++ index.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+
+import inquirer from 'inquirer';
+import chalkAnimation from 'chalk-animation';
+
+
+let attempt = 3;
+let randomNumber;
+
+const levelRules = {
+    Easy: '10',
+    Medium: '50',
+    Hard: '100',
+    SuperHard: '1000'
+}
+
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    //The maximum and minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function setRandomNumber() {
+    const level = await setLevel()
+
+    randomNumber = getRandomIntInclusive(1, levelRules[level] || levelRules.Easy)
+}
+
+async function setLevel() {
+    const level = await inquirer.prompt({
+        name: 'value',
+        type: 'list',
+        message: 'Choose level?',
+        choices: [
+            'Easy',
+            'Medium',
+            'Hard',
+            'SuperHard'
+        ]
+    });
+    return level.value
+}
+
+async function takeValue() {
+    const userValue = await inquirer.prompt({
+        name: 'entered_number',
+        type: 'input',
+        message: 'GUESS THE NUMBER?',
+        default() {
+            return '0';
+        }
+    });
+    return userValue.entered_number
+}
+
+const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms))
+
+
+async function checkNumber(num) {
+    if (num == randomNumber) {
+        const winner = chalkAnimation.rainbow('WINNER');
+        await sleep()
+        winner.stop()
+        process.exit(0);
+    } else if (num < randomNumber) {
+        console.log("TOO LOW");
+    } else {
+        console.log("TOO HIGH");
+    }
+    await startGame()
+}
+
+async function startGame() {
+    if (attempt > 0) {
+        attempt--;
+        let userNumber = await takeValue()
+        checkNumber(userNumber)
+    } else {
+        const looser = chalkAnimation.karaoke(`\nYOU LOSE! THE NUMBER WAS ${randomNumber}`);
+        await sleep()
+        looser.stop()
+        process.exit(1);
+    }
+}
+
+async function main() {
+    await setRandomNumber()
+    await startGame()
+}
+
+main()
